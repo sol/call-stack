b798bc048b00aba4700f20a7ea01b838a3f9a432
Simon Hengel <sol@typeful.net>
2016-10-03 17:05:54 +0800

    Update travis config

--- test/Util.hs
+++ test/Util.hs
@@ -1,15 +1,9 @@
 {-# LANGUAGE CPP #-}
-{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 module Util (SrcLoc(..), mapLocations) where
 
-import           Data.CallStack
-
-#if MIN_VERSION_base(4,9,0)
-import           GHC.Stack (SrcLoc(..))
-mapLocation = id
-#elif MIN_VERSION_base(4,8,1)
-
+#if MIN_VERSION_base(4,8,1) && !MIN_VERSION_base(4,9,0)
 import qualified GHC.SrcLoc as GHC
+import           Data.CallStack hiding (SrcLoc(..))
 
 data SrcLoc = SrcLoc {
   srcLocPackage :: String
@@ -21,19 +15,20 @@ data SrcLoc = SrcLoc {
 , srcLocEndCol :: Int
 } deriving (Eq, Show)
 
-mapLocation location = SrcLoc {
-  srcLocPackage = GHC.srcLocPackage location
-, srcLocModule = GHC.srcLocModule location
-, srcLocFile = GHC.srcLocFile location
-, srcLocStartLine = GHC.srcLocStartLine location
-, srcLocStartCol = GHC.srcLocStartCol location
-, srcLocEndLine = GHC.srcLocEndLine location
-, srcLocEndCol = GHC.srcLocEndCol location
-}
-
-#else
-mapLocation = id
-#endif
-
 mapLocations :: CallStack -> [(String, SrcLoc)]
 mapLocations = map (fmap mapLocation)
+  where
+    mapLocation location = SrcLoc {
+      srcLocPackage = GHC.srcLocPackage location
+    , srcLocModule = GHC.srcLocModule location
+    , srcLocFile = GHC.srcLocFile location
+    , srcLocStartLine = GHC.srcLocStartLine location
+    , srcLocStartCol = GHC.srcLocStartCol location
+    , srcLocEndLine = GHC.srcLocEndLine location
+    , srcLocEndCol = GHC.srcLocEndCol location
+    }
+#else
+import           Data.CallStack
+mapLocations :: CallStack -> CallStack
+mapLocations = id
+#endif
