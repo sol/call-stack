058c5a1485cdc36dbb52bfd5fc152f0255af87fb
Simon Hengel <sol@typeful.net>
2021-01-19 15:36:30 +0700

    Add support for GHC < 7.4.1

--- src/Data/CallStack.hs
+++ src/Data/CallStack.hs
@@ -1,11 +1,19 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ImplicitParams #-}
 
+#if __GLASGOW_HASKELL__ >= 704
+{-# LANGUAGE ConstraintKinds #-}
+#define HCS HasCallStack =>
+#else
+#define HCS
+#endif
+
 module Data.CallStack (
-  HasCallStack
-, CallStack
+#if __GLASGOW_HASKELL__ >= 704
+  HasCallStack,
+#endif
+  CallStack
 , SrcLoc(..)
 , callStack
 , callSite
@@ -22,14 +30,14 @@ import qualified GHC.Stack as GHC
 import           GHC.Stack (HasCallStack)
 #elif MIN_VERSION_base(4,8,1)
 type HasCallStack = (?callStack :: GHC.CallStack)
-#else
+#elif __GLASGOW_HASKELL__ >= 704
 import GHC.Exts (Constraint)
 type HasCallStack = (() :: Constraint)
 #endif
 
 type CallStack = [(String, SrcLoc)]
 
-callStack :: HasCallStack => CallStack
+callStack :: HCS CallStack
 #if MIN_VERSION_base(4,9,0)
 callStack = drop 1 $ GHC.getCallStack GHC.callStack
 #elif MIN_VERSION_base(4,8,1)
@@ -38,5 +46,5 @@ callStack = drop 2 $ GHC.getCallStack ?callStack
 callStack = []
 #endif
 
-callSite :: HasCallStack => Maybe (String, SrcLoc)
+callSite :: HCS Maybe (String, SrcLoc)
 callSite = listToMaybe (reverse callStack)
