312329ffe4171b2ce0b3ebc8860ecd18a8b27ff7
Dan Dart <git@dandart.co.uk>
2024-09-01 19:45:55 +0100

    Fix tests for base >= 4.18

--- test/Data/CallStackSpec.hs
+++ test/Data/CallStackSpec.hs
@@ -9,11 +9,16 @@ spec :: Spec
 spec = do
   describe "callStack" $ do
     it "returns the call stack" $ do
+#if MIN_VERSION_base(4,18,0)
+      let srcLocPackage' = "call-stack-0.4.0-inplace-spec"
+#else
+      let srcLocPackage' = "main"
+#endif
       mapLocations test `shouldBe` [
 #if MIN_VERSION_base(4,8,1)
           ("bar"
           , SrcLoc {
-              srcLocPackage = "main"
+              srcLocPackage = srcLocPackage'
             , srcLocModule = "Example"
             , srcLocFile = "test" </> "Example.hs"
             , srcLocStartLine = 18
@@ -24,7 +29,7 @@ spec = do
           )
         , ("foo"
           , SrcLoc {
-              srcLocPackage = "main"
+              srcLocPackage = srcLocPackage'
             , srcLocModule = "Example"
             , srcLocFile = "test" </> "Example.hs"
             , srcLocStartLine = 15
